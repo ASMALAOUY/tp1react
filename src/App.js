9202d01916f40e391e571f2a283cea337aec0965
Asma <a.laouy2064@uca.ac.ma>
2026-03-29 11:27:12 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import HelloWorld from './HelloWorld';
+import Greeting from './Greeting';
+import Profil from './Profil';
+import Voiture from './Voiture';
+import ListeCourses from './ListeCourses';
 
 function App() {
+  const utilisateur = { nom: 'Emma', photo: '/user.png' };
+  const courses1 = ['Pommes', 'Lait', 'Pain', 'Beurre'];
+  const courses2 = ['Stylos', 'Cahiers', 'Règle', 'Colle'];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HelloWorld />
+      <Greeting prenom="Alice" />
+      <Greeting prenom="Mohamed" />
+      <Profil utilisateur={utilisateur} taille={150} />
+      <Voiture marque="Renault" modele="Clio" couleur="Rouge" />
+      <Voiture marque="Peugeot" modele="208" couleur="Bleu" />
+      <Voiture marque="Toyota" modele="Yaris" couleur="Blanc" />
+      <ListeCourses elements={courses1} />
+      <ListeCourses elements={courses2} />
     </div>
   );
 }
